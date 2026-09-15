a09ceec863115004b39fe959bbb281035c396468
Fleury Butoyi <fleurybu@gmail.com>
2026-09-15 07:32:29 +0200

    Formatting css styles

--- src/report/BeneficiariesFollowup.tsx
+++ src/report/BeneficiariesFollowup.tsx
@@ -1,48 +1,54 @@
-import { categoryDictionary } from "../utils/Array";
+import { categoryDictionary } from '../utils/Array';
 
 const table = {
-	margin: "auto",
-	padding: "auto",
-	alignItems: "center",
-	borderCollapse: "collapse",
-	border: "1pt solid black",
+    margin: 'auto',
+    padding: 'auto',
+    alignItems: 'center',
+    borderCollapse: 'collapse',
+    border: '1pt solid black',
 } as const;
 
 function BeneficiariesFollowup(beneficiaries: Array<any>, label: string) {
-	return (
-		<div>
-			<h4>{categoryDictionary(label)}</h4>
-			<table style={table}>
-				<thead style={table}>
-					<tr style={table}>
-						<th style={table}>Name</th>
-						<th style={table}>Age</th>
-						<th style={table}>Gender</th>
-						<th style={table}>Care giver</th>
-						<th style={table}>Reg Doc</th>
-						<th style={table}>Reg number</th>
-						<th style={table}>Status</th>
-					</tr>
-				</thead>
-				<tbody style={table}>
-					{beneficiaries.map((beneficiary: any) => {
-						return (
-							<tr style={table}>
-								<td style={table}>{beneficiary.name}</td>
-								<td style={table}>{beneficiary.age}</td>
-								<td style={table}>{beneficiary.gender}</td>
-								<td style={table}>{beneficiary.careGiver}</td>
-								<td style={table}>
-									{categoryDictionary(beneficiary.registrationDocument)}
-								</td>
-								<td style={table}>{beneficiary.registrationNumber}</td>
-								<td style={table}>{categoryDictionary(beneficiary.status)}</td>
-							</tr>
-						);
-					})}
-				</tbody>
-			</table>
-		</div>
-	);
+    return (
+        <div>
+            <h4>{categoryDictionary(label)}</h4>
+            <table style={table}>
+                <thead style={table}>
+                    <tr style={table}>
+                        <th style={table}>Name</th>
+                        <th style={table}>Age</th>
+                        <th style={table}>Gender</th>
+                        <th style={table}>Care giver</th>
+                        <th style={table}>Reg Doc</th>
+                        <th style={table}>Reg number</th>
+                        <th style={table}>Status</th>
+                    </tr>
+                </thead>
+                <tbody style={table}>
+                    {beneficiaries.map((beneficiary: any) => {
+                        return (
+                            <tr style={table}>
+                                <td style={table}>{beneficiary.name}</td>
+                                <td style={table}>{beneficiary.age}</td>
+                                <td style={table}>{beneficiary.gender}</td>
+                                <td style={table}>{beneficiary.careGiver}</td>
+                                <td style={table}>
+                                    {categoryDictionary(
+                                        beneficiary.registrationDocument,
+                                    )}
+                                </td>
+                                <td style={table}>
+                                    {beneficiary.registrationNumber}
+                                </td>
+                                <td style={table}>
+                                    {categoryDictionary(beneficiary.status)}
+                                </td>
+                            </tr>
+                        );
+                    })}
+                </tbody>
+            </table>
+        </div>
+    );
 }
 export { BeneficiariesFollowup };
